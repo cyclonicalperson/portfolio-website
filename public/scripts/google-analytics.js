e31b4e29f86f09c1dd9d613841b5d83aefd3b77d
Pavle <cyclonicalperson@gmail.com>
2025-04-22 11:59:17 +0200

    WIP updates

--- public/scripts/google-analytics.js
+++ public/scripts/google-analytics.js
@@ -1,6 +1,4 @@
-(function() {
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-L0ZN882055');  // Replace with your actual Google Analytics ID
-})();
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-L0ZN882055');
